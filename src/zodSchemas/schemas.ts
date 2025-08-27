6c7ce9e20b00ee199b752de6107961217fad5cb8
spi9325 <sanketinamdar72@gmail.com>
2025-08-28 00:36:18 +0530

    added a signup zodschema

---
+++ src/zodSchemas/schemas.ts
@@ -0,0 +1,6 @@
+import z from "zod";
+
+export const signupSchema = z.object({
+    email: z.email({error:"email format is not valid..."}),
+    password: z.string().min(6,{error:"password is minimum 6"}).max(10,{error:"password is maximum 10"})
+})
