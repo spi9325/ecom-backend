1d38d57469a0ec8bc3f90aba66e9767bc8d1c367
spi9325 <sanketinamdar72@gmail.com>
2025-08-30 23:48:49 +0530

    add zod schemas

--- src/zodSchemas/schemas.ts
+++ src/zodSchemas/schemas.ts
@@ -5,3 +5,10 @@ export const signupSchema = z.object({
     password: z.string().min(6,{error:"password is minimum 6"}).max(10,{error:"password is maximum 10"}).optional(),
     image: z.string().optional()
 })
+
+export const addToCartSchema = z.object({
+      name: z.string().min(5,{error:"Name is Minimum 5"}),
+      email: z.email({error:"Provide Valid Email Format"}),
+      price: z.number().min(2,{error:"Price Minimum 2"}),
+      availabel: z.string()
+})
